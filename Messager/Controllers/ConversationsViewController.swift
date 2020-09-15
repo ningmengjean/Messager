7e5a438d8ee4fbd78dddaf26e534f2ddf207858b
wangchi <addictedtoelixir@gmail.com>
2020-09-15 10:26:45 +0800

    Wrap up getting started

--- Messager/Controllers/ConversationsViewController.swift
+++ Messager/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messager
 //
 //  Created by addictedtoelixir on 2020/9/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()

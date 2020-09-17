92254d84ff089b893be5f3f388810609fd34d883
wangchi <addictedtoelixir@gmail.com>
2020-09-17 11:00:09 +0800

    Add Login/RegisterViewController

---
+++ Messager/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Messager
+//
+//  Created by addictedtoelixir on 2020/9/15.
+//  Copyright © 2020 wangchi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+	public var width: CGFloat {
+		return self.frame.width
+	}
+	
+	public var width: CGFloat {
+		return self.frame.width
+	}
+	
+	public var width: CGFloat {
+		return self.frame.width
+	}
+	
+	public var width: CGFloat {
+		return self.frame.width
+	}
+	
+	public var width: CGFloat {
+		return self.frame.width
+	}
+	
+	public var width: CGFloat {
+		return self.frame.width
+	}
+}

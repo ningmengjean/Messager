86f4ac7bcd3f803428d862bd652e2a9a0f002bff
wangchi <addictedtoelixir@gmail.com>
2020-09-23 20:05:08 +0800

    add database

--- Messager/Controllers/Login/LoginViewController.swift
+++ Messager/Controllers/Login/LoginViewController.swift
@@ -7,24 +7,153 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
+	
+	private let scrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.clipsToBounds = true
+		return scrollView
+	}()
+	
+	private let imageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.image = UIImage(named: "logo")
+		imageView.contentMode = .scaleAspectFit
+		return imageView
+	}()
+	
+	private let emailField: UITextField = {
+		let field = UITextField()
+		field.autocapitalizationType = .none
+		field.autocorrectionType = .no
+		field.returnKeyType = .continue
+		field.layer.cornerRadius = 12
+		field.layer.borderWidth = 1
+		field.layer.borderColor = UIColor.lightGray.cgColor
+		field.placeholder = "Email Address..."
+		field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+		field.leftViewMode = .always
+		field.backgroundColor = .white
+		return field
+	}()
+	
+	private let passwordField: UITextField = {
+		let field = UITextField()
+		field.autocapitalizationType = .none
+		field.autocorrectionType = .no
+		field.returnKeyType = .done
+		field.layer.cornerRadius = 12
+		field.layer.borderWidth = 1
+		field.layer.borderColor = UIColor.lightGray.cgColor
+		field.placeholder = "Password..."
+		field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+		field.leftViewMode = .always
+		field.backgroundColor = .white
+		field.isSecureTextEntry = true
+		return field
+	}()
+	
+	private let loginButton: UIButton = {
+		let button = UIButton()
+		button.setTitle("Log in", for: .normal)
+		button.backgroundColor = .link
+		button.setTitleColor(.white, for: .normal)
+		button.layer.cornerRadius = 12
+		button.layer.masksToBounds = true
+		button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+		return button
+	}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		title = "Log in"
+		view.backgroundColor = .white
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+															style: .done,
+															target: self,
+															action: #selector(didTapRegister))
+		loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+		//Add subviews
+		view.addSubview(scrollView)
+		scrollView.addSubview(imageView)
+		scrollView.addSubview(emailField)
+		scrollView.addSubview(passwordField)
+		scrollView.addSubview(loginButton)
+		
+		emailField.delegate = self
+		passwordField.delegate = self
     }
-    
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		scrollView.frame = view.bounds
+		let size = scrollView.width/3
+		imageView.frame = CGRect(x: (scrollView.width-size)/2,
+								 y: 20,
+								 width: size,
+								 height: size)
+		emailField.frame = CGRect(x: 30,
+								  y: imageView.bottom+10,
+								  width: scrollView.width-60,
+								  height: 52)
+		passwordField.frame = CGRect(x: 30,
+								  y: emailField.bottom+10,
+								  width: scrollView.width-60,
+								  height: 52)
+		loginButton.frame = CGRect(x: 30,
+								   y: passwordField.bottom+10,
+								   width: scrollView.width-60,
+								   height: 52)
+	}
+	
+	@objc private func loginButtonTapped() {
+		emailField.resignFirstResponder()
+		passwordField.resignFirstResponder()
+		guard  let email = emailField.text, let password = passwordField.text, !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+			alertUserLoginError()
+			return
+		}
+		
+		// Firebase Log In
+		FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { [weak self] authResult, error in
+			guard let strongSelf = self else {
+				return
+			}
+			guard let result = authResult, error == nil else {
+				print("Failed to log in user with email: \(email)")
+				return
+			}
+			let user = result.user
+			print("Logged In User: \(user)")
+			strongSelf.navigationController?.dismiss(animated: true, completion: nil)
+		})
+	}
+	
+	func alertUserLoginError() {
+		let  alert = UIAlertController(title: "Woops...",
+									   message: "Please enter all the informations.",
+									   preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+		present(alert, animated: true)
+	}
+	
+	@objc private func didTapRegister() {
+		let vc = RegisterViewController()
+		vc.title = "Creat Account"
+		navigationController?.pushViewController(vc, animated: true)
+	}
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension LoginViewController: UITextFieldDelegate {
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		if textField == emailField {
+			passwordField.becomeFirstResponder()
+		} else if textField == passwordField {
+			loginButtonTapped()
+		}
+		return true 
+	}
 }
--- Podfile
+++ Podfile
@@ -1,10 +1,13 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+# platform :ios, '13.0'
 
 target 'Messager' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for Messager
+# Firebase
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
 
 end
